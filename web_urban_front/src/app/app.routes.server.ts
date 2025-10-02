99f0b5e9c791bb2e2ce048f71bf5736c4bc17d2c
Olczak_Mikolaj <01159104@pw.edu.pl>
2025-10-02 21:57:23 +0200

    created containers setup

--- web_urban_front/src/app/app.routes.server.ts
+++ web_urban_front/src/app/app.routes.server.ts
@@ -2,6 +2,17 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'error/:code',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [
+        { code: '404' },
+        { code: '403' },
+        { code: '500' }
+      ];
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
